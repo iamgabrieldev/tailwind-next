676aaa0fbf506dda46574e0e1635b35804c93ef6
Gabriel Oliveira <gabriellfernandoo@gmail.com>
2024-01-25 11:00:34 -0300

    feat: add select country and timezone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { SettingsTabs } from '@/components/SettingsTabs';
 import * as Input from '@/components/input';
-import { Mail, UploadCloud, User } from 'lucide-react';
+import * as FileInput from '@/components/Form/FileInput';
+import { Mail } from 'lucide-react';
+import { Select } from '@/components/Form/Select';
+import { SelectItem } from '@/components/Form/Select/SelectItem';
+
 
 export default function Home() {
   return (
@@ -70,34 +74,11 @@ export default function Home() {
               <span className='mt-0.5 text-sm font-normal text-zinc-500 block'>This will be displayed on your profile.</span> 
             </label>
 
-            <div className="flex items-start gap-5">
-              <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
-                <User className="h-8 w-8 text-violet-500" />
-              </div>
-
-              <label
-                htmlFor="photo"
-                className="group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500"
-              >
-                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
-                  <UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
-                </div>
-
-                <div className="flex flex-col items-center gap-1">
-                  <span className="text-sm">
-                    <span className="font-semibold text-violet-700">
-                      Click to upload
-                    </span>{' '}
-                    or drag and drop
-                  </span>
-                  <span className="text-xs">
-                    SVG, PNG, JPG or GIF (max. 800x400px)
-                  </span>
-                </div>
-              </label>
-
-              <input type="file" className="sr-only" id="photo" />
-            </div>
+            <FileInput.Root className="flex items-start gap-5">
+              <FileInput.ImagePreview />
+              <FileInput.Trigger />
+              <FileInput.Control />
+            </FileInput.Root>
           </div>
 
           <div className='grid gap-3 grid-cols-form pt-5'>
@@ -119,7 +100,12 @@ export default function Home() {
             >
               Country
             </label>
-           <div></div>
+
+            <Select placeholder="Select a country...">
+              <SelectItem value="br" text="Brazil" />
+              <SelectItem value="us" text="United States" />
+            </Select>
+          
           </div>
 
           <div className='grid gap-3 grid-cols-form pt-5'>
@@ -129,17 +115,15 @@ export default function Home() {
             >
               Timezone
             </label>
-            <div></div>
-          </div>
 
-          <div className='grid gap-3 grid-cols-form pt-5'>
-            <label 
-              htmlFor="timezone" 
-              className='text-sm font-medium text-zinc-700'
-            >
-              Timezone
-            </label>
-            <div></div>
+            <Select placeholder="Select a timezone...">
+              <SelectItem
+                value="utc8"
+                text="Pacific Standard Time (UTC-08:00)"
+              />
+              <SelectItem value="utc3" text="America São Paulo (UTC-03:00)" />
+            </Select>
+            
           </div>
 
           <div className='grid gap-3 grid-cols-form pt-5'>
@@ -165,7 +149,11 @@ export default function Home() {
                 Share a few snippets of your work.
               </span> 
             </label>
-            <div></div>
+            <FileInput.Root>
+              <FileInput.Trigger />
+              <FileInput.FileList />
+              <FileInput.Control multiple />
+            </FileInput.Root>
           </div>
 
           <div className='flex items-center justify-end pt-5 gap-2'>
